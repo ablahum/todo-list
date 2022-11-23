66386490f0164d4753471191f1e1a1e2437d6bfe
ablahum <mriztama@gmail.com>
2022-11-24 01:45:37 +0700

    fix bug

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,30 +1,3 @@
-const Button = ({ type, handler }) => {
-  const iconType = () => {
-    switch (type) {
-      case 'complete':
-        return <i className='fa fa-check-circle'></i>
-        break
-      case 'edit':
-        return <i className='fa fa-edit'></i>
-        break
-      case 'delete':
-        return <i className='fa fa-trash'></i>
-        break
-
-      default:
-        return <h1>No project match</h1>
-    }
-  }
-
-  // console.log(iconType)
-
-  return (
-    <button className={`button-${type} task-button`} onClick={() => handler()}>
-      {iconType}
-    </button>
-  )
-}
-
 const TodoList = ({ todos, setTodos, setEditTodo }) => {
   const handleComplete = (todo) => {
     setTodos(
@@ -54,11 +27,9 @@ const TodoList = ({ todos, setTodos, setEditTodo }) => {
         <li className='list-item' key={todo.id}>
           <input type='text' value={todo.title} className={` list ${todo.completed ? 'complete' : ''}`} onChange={(e) => e.preventDefault()} />
 
-          <Button type='complete' handler={handleComplete(todo)} />
-
-          {/* <button className='button-complete task-button' onClick={() => handleComplete(todo)}>
+          <button className='button-complete task-button' onClick={() => handleComplete(todo)}>
             <i className='fa fa-check-circle'></i>
-          </button> */}
+          </button>
 
           <button className='button-edit task-button' onClick={() => handleEdit(todo)}>
             <i className='fa fa-edit'></i>
